b6e34b45b3991dafd68feab1b29232e455969903
HeeMang-Lee <hemsej018@naver.com>
2025-05-22 09:48:59 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 16156 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 입력 스트림
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 출력 스트림
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCount = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= testCount; i++) {
+            String[] input = br.readLine().split(" ");
+            int a = Integer.parseInt(input[0]);
+            int b = Integer.parseInt(input[1]);
+
+            bw.write("Case #" + i + ": " + (a + b) + "\n");
+        }
+
+        // 출력 비우고 자원 닫기
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
