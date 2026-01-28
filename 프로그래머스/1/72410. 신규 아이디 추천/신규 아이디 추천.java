f3855394b1edb62ae316dab56c4cb966d3e419bf
HeeMang-Lee <hemsej018@naver.com>
2026-01-28 20:06:21 +0900

    [level 1] Title: 신규 아이디 추천, Time: 14.24 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String new_id) {
+        String id = new_id.toLowerCase();
+        id = id.replaceAll("[^a-z0-9-_.]","");
+        id = id.replaceAll("\\.+", ".");
+        id = id.replaceAll("^\\.|\\.$", "");
+        if(id.isEmpty()) {
+            id = "a";
+        }
+        if(id.length() >= 16) {
+            id = id.substring(0,15);
+            id = id.replaceAll("\\.$", "");
+        }
+        while (id.length() < 3) {
+            id += id.charAt(id.length() - 1);
+        }
+        return id;
+    }
+}
