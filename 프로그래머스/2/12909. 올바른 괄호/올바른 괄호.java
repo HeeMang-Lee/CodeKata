83a9e1e26ecfc083ae6c515a6afe347cb90c96aa
HeeMang-Lee <hemsej018@naver.com>
2026-05-01 07:27:21 +0900

    [level 2] Title: 올바른 괄호, Time: 8.85 ms, Memory: 54.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') count++;
+            if (s.charAt(i) == ')') count--;
+            if (count < 0) return false;
+        }
+        
+        if (count != 0) return false;
+        
+        return answer;
+    }
+}
