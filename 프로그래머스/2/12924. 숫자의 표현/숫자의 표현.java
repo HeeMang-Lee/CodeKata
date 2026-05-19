a933316ad78ab646dacba0e139ecccbbb3b82853
HeeMang-Lee <hemsej018@naver.com>
2026-05-19 10:53:35 +0900

    [level 2] Title: 숫자의 표현, Time: 0.30 ms, Memory: 56.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int start = 1, end = 1;
+        int sum = 1;
+        while (start <= n) {
+            if (sum == n) {
+                answer++;
+                end++;
+                sum += end;
+            }
+            else if (sum < n) {
+                end++;
+                sum += end;
+            }
+            else {
+                sum -= start;
+                start++;
+            }
+        }
+        return answer;
+    }
+}
