a96919500a80aa8397cf1e3365ad23a8e535a730
HeeMang-Lee <hemsej018@naver.com>
2026-06-07 21:05:24 +0900

    [level 1] Title: 추억 점수, Time: 1.13 ms, Memory: 72.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < name.length; i++) {
+            String s = name[i];
+            int year = yearning[i];
+            map.put(s,year);
+        }
+        int[] answer = new int[photo.length];
+        for (int i = 0; i < photo.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < photo[i].length; j++) {
+                sum += map.getOrDefault(photo[i][j],0);
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}
