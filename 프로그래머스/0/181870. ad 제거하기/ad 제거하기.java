d206eefb48be1d03eaccdc3a4e573c89a1f2786f
HeeMang-Lee <hemsej018@naver.com>
2026-06-20 21:23:29 +0900

    [level 0] Title: ad 제거하기, Time: 0.49 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        ArrayList<String> list = new ArrayList<>();
+        for (String arr : strArr) {
+            if(!arr.contains("ad")) list.add(arr);
+        }
+        return list.toArray(new String[0]);
+    }
+}
